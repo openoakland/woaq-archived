ccbedc56580ca03434655e39a89f8d92001d593d
Allan Peng <potette007@gmail.com>
2018-02-27 20:21:52 -0800

    cleanup

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# create the db
-echo "CREATE DATABASE IF NOT EXISTS woiep" | mysql -u root -ppassword
-
-# run the two load scripts
-mysql -u root -ppassword woiep < pm.mysql
-mysql -u root -ppassword woiep < pmload.mysql
