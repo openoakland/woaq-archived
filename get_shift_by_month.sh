24169bfa519a54cce1ac050fd9d09c9c1b1bac3f
Allan Peng <potette007@gmail.com>
2018-05-20 22:30:18 -0700

    group CSVs by month

---
+++ get_shift_by_month.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p shift_by_month
+
+rm -rf shift_by_month/*
+
+for pref in `ls shifts  | cut -c1-10 | uniq`; do
+    # We want the header from the first CSV, but not the remaining ones
+    skip_line=0
+    for file in `ls shifts/$pref*`; do
+        tail -n +$skip_line $file >> shift_by_month/$pref
+        skip_line=2
+    done
+    echo $pref
+done
