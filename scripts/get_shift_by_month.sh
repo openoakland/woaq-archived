651b184a87c29c4d92651ff5388f81e844bd0ca8
Allan Peng <potette007@gmail.com>
2018-05-22 19:55:47 -0700

    hacky scripts to merge everythign

--- scripts/get_shift_by_month.sh
+++ scripts/get_shift_by_month.sh
@@ -4,16 +4,15 @@ mkdir -p shift_by_month
 
 rm -rf shift_by_month/*
 
-for pref in `ls shifts  | cut -c1-10 | uniq`; do
+for pref in `ls shifts  | cut -c4-10 | sort | uniq`; do
     # We want the header from the first CSV, but not the remaining ones
     skip_line=0
-    for file in `ls shifts/$pref*`; do
+    for file in `ls shifts/*$pref*`; do
         tail -n +$skip_line $file >> shift_by_month/$pref
         skip_line=2
     done
     echo $pref
 done
-
 
 # this is terrible, i know.
 cd shift_by_month
@@ -21,3 +20,4 @@ cd shift_by_month
 for file in *; do
     mv -- "$file" "${file//[/}.csv"
 done
+
--- scripts/make_markdown.sh
+++ scripts/make_markdown.sh
@@ -5,14 +5,21 @@ posts_dir=_posts
 
 DATE=`date +%Y-%m-%d`
 
-for file in `ls shifts_by_month`; do
-    base=`echo file | cut -c1-9`
+for file in `ls ./shift_by_month`; do
+    base=`echo $file | cut -c1-7`
+    echo $base
     md_file=$DATE-$base.markdown
+    YEAR=`echo $base | cut -c1-4`
+    MONTH=`echo $base | cut -c6-7`
     echo "---" > $posts_dir/$md_file
+    echo "title: Citizen Science ${MONTH}-${YEAR}" >> $posts_dir/$md_file
+    echo "owner: WOEIP" >> $posts_dir/$md_file
+    echo "layout: data" >> $posts_dir/$md_file
+    echo "month: ${MONTH}" >> $posts_dir/$md_file
+    echo "year: ${YEAR}" >> $posts_dir/$md_file
     echo "categories: citizenScience" >> $posts_dir/$md_file
-    echo "URL: http://s3-us-west-2.amazonaws.com/openoakland-woaq/shift_by_month/$file" \
-        >>$posts_dir/$md_file
+    echo "fileName: <a href=\"http://s3-us-west-2.amazonaws.com/openoakland-woaq/shift_by_month/$file\">CSV here</a>" \
+        >> $posts_dir/$md_file
     echo "---" >> $posts_dir/$md_file
-    
 
 done
