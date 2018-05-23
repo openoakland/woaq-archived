ddea21801c853e6c407b1824123035fdc1e71d14
Allan Peng <potette007@gmail.com>
2018-05-22 20:15:41 -0700

    apologies

--- scripts/get_shift_by_month.sh
+++ scripts/get_shift_by_month.sh
@@ -2,22 +2,22 @@
 
 mkdir -p shift_by_month
 
-rm -rf shift_by_month/*
-
+# OK i know this is hacky, but bear with me.
+# This is the state of our world right now.
+# The time readings within each data set are
+# only avaiable via UTC time stamps. 
+# I'm not good enough with SQL to figure out how to
+# get the sql query to automatically extract months for us.
+# So we just cut out the relevant characters from the names of 
+# the file and use that to match files from the same dates.
 for pref in `ls shifts  | cut -c4-10 | sort | uniq`; do
-    # We want the header from the first CSV, but not the remaining ones
+
+    # We want to print the CSV header only once.
     skip_line=0
     for file in `ls shifts/*$pref*`; do
-        tail -n +$skip_line $file >> shift_by_month/$pref
+        tail -n +$skip_line $file >> shift_by_month/$pref.csv
         skip_line=2
     done
     echo $pref
-done
-
-# this is terrible, i know.
-cd shift_by_month
-
-for file in *; do
-    mv -- "$file" "${file//[/}.csv"
 done
 
--- scripts/make_markdown.sh
+++ scripts/make_markdown.sh
@@ -5,21 +5,24 @@ posts_dir=_posts
 
 DATE=`date +%Y-%m-%d`
 
+# Make a separate page for each file in.
 for file in `ls ./shift_by_month`; do
     base=`echo $file | cut -c1-7`
     echo $base
-    md_file=$DATE-$base.markdown
+    md_file=$posts_dir/$DATE-$base.markdown
     YEAR=`echo $base | cut -c1-4`
     MONTH=`echo $base | cut -c6-7`
-    echo "---" > $posts_dir/$md_file
-    echo "title: Citizen Science ${MONTH}-${YEAR}" >> $posts_dir/$md_file
-    echo "owner: WOEIP" >> $posts_dir/$md_file
-    echo "layout: data" >> $posts_dir/$md_file
-    echo "month: ${MONTH}" >> $posts_dir/$md_file
-    echo "year: ${YEAR}" >> $posts_dir/$md_file
-    echo "categories: citizenScience" >> $posts_dir/$md_file
+
+    # Write yaml file line by line
+    echo "---" > $md_file
+    echo "title: Citizen Science ${MONTH}-${YEAR}" >> $md_file
+    echo "owner: WOEIP" >> $md_file
+    echo "layout: data" >> $md_file
+    echo "month: ${MONTH}" >> $md_file
+    echo "year: ${YEAR}" >> $md_file
+    echo "categories: citizenScience" >> $md_file
     echo "fileName: <a href=\"http://s3-us-west-2.amazonaws.com/openoakland-woaq/shift_by_month/$file\">CSV here</a>" \
-        >> $posts_dir/$md_file
-    echo "---" >> $posts_dir/$md_file
+        >> $md_file
+    echo "---" >> $md_file
 
 done
