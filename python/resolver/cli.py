ee3cd2ddd934587261354010d9f6c5f85d68db10
Kuan Butts <kuanbutts@gmail.com>
2018-06-05 20:55:36 -0700

    simple init files, placeholder cli scaffolding

---
+++ python/resolver/cli.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+	# TODO: The cli.py will include arg parsing and related
+	#       script components to instantiate the Resolver class with
+	# 		relation initialization components. It will, once initialized
+	# 		also call the transform and load processes, enabling
+	#       command-line scripted ETL process abstraction.
+	print('WIP')
